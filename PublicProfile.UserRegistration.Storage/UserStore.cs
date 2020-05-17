fa68d191d9fa79e56e0c62c406e89acf9f928575
Natalia Epimakhova <epimakhova@gmail.com>
2020-05-16 19:57:11 -0700

    Adding stub for Azure tables based storage

---
+++ PublicProfile.UserRegistration.Storage/UserStore.cs
@@ -0,0 +1,38 @@
+﻿namespace PublicProfile.UserRegistration.Storage
+{
+    using Microsoft.AspNet.Identity;
+    using System.Threading.Tasks;
+
+    public class UserStore<TUser> : IUserStore<TUser> where TUser : class, IUser<string>
+    {
+        public Task CreateAsync(TUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(TUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TUser> FindByIdAsync(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TUser> FindByNameAsync(string userName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(TUser user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
